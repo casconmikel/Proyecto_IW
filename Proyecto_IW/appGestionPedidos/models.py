8417c63a7533fc1d297ed47d1152706013d9a610
gdomaica <guillermo.domaica@opendeusto.es>
2023-04-25 19:06:34 +0200

    Modificando modelos para no tener problema en DB

--- Proyecto_IW/appGestionPedidos/models.py
+++ Proyecto_IW/appGestionPedidos/models.py
@@ -1,45 +1,54 @@
+from enum import unique
 from django.db import models
  
 class Cliente(models.Model):
-    CIF = models.CharField(max_length=9)
+    CIF = models.CharField(max_length=9, unique=True)
     nombreEmpresa = models.CharField(max_length=16)
     direccion = models.CharField(max_length=60)
     datosContacto = models.CharField(max_length=60)
 
     def __str__(self):
-       return f"{self.id} - {self.CIF} - {self.nombreEmpresa} - {self.direccion} - {self.datosContacto}" 
+       return f"{self.id} - {self.CIF} - {self.nombreEmpresa}" 
 
-class Pedido(models.Model):
-    fecha = models.DateField()
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE) 
-    precioTotal = models.FloatField()
+
+class Componente(models.Model): 
+    codigoReferencia = models.IntegerField(unique=True)
+    nombreModelo = models.CharField(max_length=16)
+    marca = models.CharField(max_length=16)
 
     def __str__(self):
-       return f"{self.id} - {self.fecha} - {self.cliente} - {self.precioTotal}" 
+       return f"{self.id} - {self.codigoReferencia} - {self.nombreModelo}"
+
 
 class Producto(models.Model):
+    referencia = models.IntegerField(unique=True)
     precio = models.FloatField()
     nombre = models.CharField(max_length=16)
     descripcion = models.CharField(max_length=60)
     categoria = models.CharField(max_length=16)
-    pedido = models.ManyToManyField(Pedido)    
+    componente = models.ManyToManyField(Componente) 
 
     def __str__(self):
-       return f"{self.id} - {self.precio} - {self.nombre} - {self.descripcion} - {self.categoria} - {self.pedido}" 
+       return f"{self.id} - {self.precio} - {self.nombre} - {self.categoria}" 
 
-class Componente(models.Model): 
-    codigoReferencia = models.IntegerField()
-    nombreModelo = models.CharField(max_length=16)
-    marca = models.CharField(max_length=16)
-    producto = models.ManyToManyField(Producto)
+
+class Pedido(models.Model):
+    referencia = models.IntegerField(unique=True)
+    producto = models.ManyToManyField(Producto, through="ProductoPedido")
+    fecha = models.DateField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE) 
+    precioTotal = models.FloatField(blank=True, null=True)
 
     def __str__(self):
-       return f"{self.id} - {self.codigoReferencia} - {self.nombreModelo} - {self.marca} - {self.producto}" 
+       return f"{self.id} - {self.fecha} - {self.cliente}" 
+
+ 
 
 class ProductoPedido(models.Model):
-    idProducto = models.ManyToManyField(Producto)
-    idPedido = models.ManyToManyField(Pedido)
+    idProducto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    idPedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
     cantidad = models.IntegerField()
+
 
     def __str__(self):
        return f"{self.id} - {self.idProducto} - {self.idPedido} - {self.cantidad}" 
